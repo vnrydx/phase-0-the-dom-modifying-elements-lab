b41027b16f85a61469506a8841a4bccb2b2f82ba
vnrydx <vnrydx@gmail.com>
2022-11-22 14:50:47 -0600

    commit tree f8a745301efc8103742d4722317756e798bc78b5

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "HEY is the champion";
